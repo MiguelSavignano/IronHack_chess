d02e547f40ab4d55c0eed34162a8717a343debb4
masx <migue.masx@gmail.com>
2015-04-10 12:39:05 +0200

    arfil  reina implementados downcase.ord tranforma la letra en numero

--- Ajedres.rb
+++ Ajedres.rb
@@ -10,22 +10,44 @@ class ValidatorTable
 	end
 end
 class ValidatorFicha
-	attr_accessor :origin
-	attr_accessor :to_go
+	attr_accessor :origin, :to_go
+	attr_accessor :origin_vertical_move ,:origin_hotizontal_move
+	attr_accessor :to_go_vertical_move, :to_go_hotizontal_move
 	def initialize(origin)
-		@origin_vertical_move=origin[0]
-		@origin_hotizontal_move=origin[1]
+		@origin_vertical_move=origin[0].downcase.ord-96
+		@origin_hotizontal_move=origin[1].to_i 
 	end
 end
 class ValidatorTorre <ValidatorFicha
 	def can_move_to(to_go)
-		@to_go_vertical_move=to_go[0]
-		@to_go_hotizontal_move=to_go[1]
-		return true if @origin_vertical_move==@to_go_vertical_move
-		return true if @origin_hotizontal_move==@to_go_hotizontal_move
+		@to_go_vertical_move=to_go[0].downcase.ord-96
+		@to_go_hotizontal_move=to_go[1].to_i 
+		return true if @origin_vertical_move==@to_go_vertical_move || @origin_hotizontal_move==@to_go_hotizontal_move
+		return false
+	end
+end
+class ValidatorArfil <ValidatorFicha
+	def can_move_to(to_go)
+		@to_go_vertical_move=to_go[0].downcase.ord-96
+		@to_go_hotizontal_move=to_go[1].to_i
+		return  @to_go_vertical_move-@origin_vertical_move==@to_go_hotizontal_move-@origin_hotizontal_move
+	end
+end
+class ValidatorReina <ValidatorFicha
+	def can_move_to(to_go)
+		@to_go_vertical_move=to_go[0].downcase.ord-96
+		@to_go_hotizontal_move=to_go[1].to_i 
+		return true if @origin_vertical_move==@to_go_vertical_move || @origin_hotizontal_move==@to_go_hotizontal_move
+		return true if @to_go_vertical_move-@origin_vertical_move==@to_go_hotizontal_move-@origin_hotizontal_move
 		return false
 	end
 end
 torre=ValidatorTorre.new("D2") 
+arfil=ValidatorArfil.new("D2") 
+reina=ValidatorReina.new("D2") 
 torre.can_move_to("D8") 
+arfil.can_move_to("D8") 
+reina.can_move_to("D8") 
 binding.pry
+puts ""
+#"f".downcase.ord-96 
