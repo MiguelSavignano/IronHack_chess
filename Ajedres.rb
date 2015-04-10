437fa8ba1d316a30c5ec40bbd105a970c279c6fe
masx <migue.masx@gmail.com>
2015-04-10 09:54:49 +0200

    ValidatorTorre return true or nil

--- Ajedres.rb
+++ Ajedres.rb
@@ -10,15 +10,21 @@ class ValidatorTable
 	end
 end
 class ValidatorTorre
+	attr_accessor :origin
+	attr_accessor :to_go
 	def initialize(origin)
-		@origin
+		@origin=origin
 	end
 	def can_move_to(to_go)
-		@origin_vertical_move[0]=@origin[0]
-		@origin_vertical_move[1]=@origin[1]
-		@to_go_vertical_move[0]=@origin[0]
-		@to_go_hotizontal_move[1]=@origin[1]
-		return true if @origin_vertical_move==to_go_vertical_move
-		return true if @origin_hotizontal_move==to_go_hotizontal_move
+		@to_go=to_go
+		@origin_vertical_move=@origin[0]
+		@to_go_vertical_move=@to_go[0]
+		@origin_hotizontal_move=@origin[1]
+		@to_go_hotizontal_move=@to_go[1]
+		return true if @origin_vertical_move==@to_go_vertical_move
+		return true if @origin_hotizontal_move==@to_go_hotizontal_move
 	end
 end
+
+torre=ValidatorTorre.new("D2") 
+torre.can_move_to("D8") 
