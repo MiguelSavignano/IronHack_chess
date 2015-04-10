72abe760370b13cbfe98232d4d35dd24a5610bee
masx <migue.masx@gmail.com>
2015-04-10 10:12:57 +0200

    refactor validator father validatorficha initialize verticalmove an horizontal move

--- Ajedres.rb
+++ Ajedres.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Player
 	def move_to(origin,to_go)
 		return true
@@ -6,25 +7,25 @@ class Player
 end
 class ValidatorTable
 	def out_table
-		
 	end
 end
-class ValidatorTorre
+class ValidatorFicha
 	attr_accessor :origin
 	attr_accessor :to_go
 	def initialize(origin)
-		@origin=origin
-	end
-	def can_move_to(to_go)
-		@to_go=to_go
-		@origin_vertical_move=@origin[0]
-		@to_go_vertical_move=@to_go[0]
-		@origin_hotizontal_move=@origin[1]
-		@to_go_hotizontal_move=@to_go[1]
-		return true if @origin_vertical_move==@to_go_vertical_move
-		return true if @origin_hotizontal_move==@to_go_hotizontal_move
+		@origin_vertical_move=origin[0]
+		@origin_hotizontal_move=origin[1]
 	end
 end
-
+class ValidatorTorre <ValidatorFicha
+	def can_move_to(to_go)
+		@to_go_vertical_move=to_go[0]
+		@to_go_hotizontal_move=to_go[1]
+		return true if @origin_vertical_move==@to_go_vertical_move
+		return true if @origin_hotizontal_move==@to_go_hotizontal_move
+		return false
+	end
+end
 torre=ValidatorTorre.new("D2") 
 torre.can_move_to("D8") 
+binding.pry
