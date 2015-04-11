34584ab1caa0e94ec263867264204023f5dbb7d7
masx <migue.masx@gmail.com>
2015-04-11 02:21:22 +0200

    Piece implemente move - method print print original hash : solution of nil in the hash and call draw_piece is add method draw_piece and whe you call draw_piece in a nil object print -

--- Chess.rb
+++ Chess.rb
@@ -1,4 +1,9 @@
 require 'pry'
+class NilClass
+	def draw_piece
+		"-"
+	end
+end
 module MoveHelper
 		def move_tower(piece_origin,piece_to_go)
 			  change_to_i(piece_origin,piece_to_go)
@@ -12,36 +17,37 @@ module MoveHelper
 			change_to_i(piece_origin,piece_to_go)
 			 (@to_go_vertical_move-@origin_vertical_move).abs<=2 && (@to_go_hotizontal_move-@origin_hotizontal_move).abs<=2
 		end
+		def move_king(piece_origin,piece_to_go)
+			change_to_i(piece_origin,piece_to_go)
+			 (@to_go_vertical_move-@origin_vertical_move).abs<=1 
+		end
 end
 class Board
-	attr_accessor :hash_board, :piece_to_go, :piece_to_go, :piece_in_board
+	attr_accessor :board, :piece_to_go, :piece_to_go, :piece_in_board
 	def initialize(hash_board)
-		@hash_board=hash_board
-		@board_to_print={}
-		@hash_board.each do |key,value|
-			if @hash_board[key]==nil
-				@board_to_print[key]="-"
-			else
-				@board_to_print[key]=value.draw_piece
-			end
-		end
-		def print
-			puts "#{@board_to_print["A8"]} #{@board_to_print["B8"]} #{@board_to_print["C8"]} #{@board_to_print["D8"]} #{@board_to_print["E8"]} #{@board_to_print["F8"]} #{@board_to_print["G8"]} #{@board_to_print["H8"]}"
-			puts "#{@board_to_print["A7"]} #{@board_to_print["B7"]} #{@board_to_print["C7"]} #{@board_to_print["D7"]} #{@board_to_print["E7"]} #{@board_to_print["F7"]} #{@board_to_print["G7"]} #{@board_to_print["H7"]}"
-			puts "#{@board_to_print["A6"]} #{@board_to_print["B6"]} #{@board_to_print["C6"]} #{@board_to_print["D6"]} #{@board_to_print["E6"]} #{@board_to_print["F6"]} #{@board_to_print["G6"]} #{@board_to_print["H6"]}"
-			puts "#{@board_to_print["A5"]} #{@board_to_print["B5"]} #{@board_to_print["C5"]} #{@board_to_print["D5"]} #{@board_to_print["E5"]} #{@board_to_print["F5"]} #{@board_to_print["G5"]} #{@board_to_print["H5"]}"
-			puts "#{@board_to_print["A4"]} #{@board_to_print["B4"]} #{@board_to_print["C4"]} #{@board_to_print["D4"]} #{@board_to_print["E4"]} #{@board_to_print["F4"]} #{@board_to_print["G4"]} #{@board_to_print["H4"]}"
-			puts "#{@board_to_print["A3"]} #{@board_to_print["B3"]} #{@board_to_print["C3"]} #{@board_to_print["D3"]} #{@board_to_print["E3"]} #{@board_to_print["F3"]} #{@board_to_print["G3"]} #{@board_to_print["H3"]}"
-			puts "#{@board_to_print["A2"]} #{@board_to_print["B2"]} #{@board_to_print["C2"]} #{@board_to_print["D2"]} #{@board_to_print["E2"]} #{@board_to_print["F2"]} #{@board_to_print["G2"]} #{@board_to_print["H2"]}"
-			puts "#{@board_to_print["A1"]} #{@board_to_print["B1"]} #{@board_to_print["C1"]} #{@board_to_print["D1"]} #{@board_to_print["E1"]} #{@board_to_print["F1"]} #{@board_to_print["G1"]} #{@board_to_print["H1"]}"
-		end
+		@empty_position="-"
+		@board=hash_board
 	end
-	def move_piece(piece_origin,piece_to_go)
-		piece_in_board=@hash_board[piece_origin]
-		piece_in_board.can_move_to(piece_origin,piece_to_go) if piece_in_board != nil
+	def print
+			puts "#{@board["A8"].draw_piece} #{@board["B8"].draw_piece} #{@board["C8"].draw_piece} #{@board["D8"].draw_piece} #{@board["E8"].draw_piece} #{@board["F8"].draw_piece} #{@board["G8"].draw_piece} #{@board["H8"].draw_piece}"
+			puts "#{@board["A7"].draw_piece} #{@board["B7"].draw_piece} #{@board["C7"].draw_piece} #{@board["D7"].draw_piece} #{@board["E7"].draw_piece} #{@board["F7"].draw_piece} #{@board["G7"].draw_piece} #{@board["H7"].draw_piece}"
+			puts "#{@board["A6"].draw_piece} #{@board["B6"].draw_piece} #{@board["C6"].draw_piece} #{@board["D6"].draw_piece} #{@board["E6"].draw_piece} #{@board["F6"].draw_piece} #{@board["G6"].draw_piece} #{@board["H6"].draw_piece}"
+			puts "#{@board["A5"].draw_piece} #{@board["B5"].draw_piece} #{@board["C5"].draw_piece} #{@board["D5"].draw_piece} #{@board["E5"].draw_piece} #{@board["F5"].draw_piece} #{@board["G5"].draw_piece} #{@board["H5"].draw_piece}"
+			puts "#{@board["A4"].draw_piece} #{@board["B4"].draw_piece} #{@board["C4"].draw_piece} #{@board["D4"].draw_piece} #{@board["E4"].draw_piece} #{@board["F4"].draw_piece} #{@board["G4"].draw_piece} #{@board["H4"].draw_piece}"
+			puts "#{@board["A3"].draw_piece} #{@board["B3"].draw_piece} #{@board["C3"].draw_piece} #{@board["D3"].draw_piece} #{@board["E3"].draw_piece} #{@board["F3"].draw_piece} #{@board["G3"].draw_piece} #{@board["H3"].draw_piece}"
+			puts "#{@board["A2"].draw_piece} #{@board["B2"].draw_piece} #{@board["C2"].draw_piece} #{@board["D2"].draw_piece} #{@board["E2"].draw_piece} #{@board["F2"].draw_piece} #{@board["G2"].draw_piece} #{@board["H2"].draw_piece}"
+			puts "#{@board["A1"].draw_piece} #{@board["B1"].draw_piece} #{@board["C1"].draw_piece} #{@board["D1"].draw_piece} #{@board["E1"].draw_piece} #{@board["F1"].draw_piece} #{@board["G1"].draw_piece} #{@board["H1"].draw_piece}"
 	end
 	def piece(piece_search)
-		piece_in_board=@hash_board[piece_search]
+		piece_in_board=@board[piece_search].draw_piece
+	end
+	def move_piece(position_origin,position_to_go)
+		piece_in_board=@board[position_origin]
+		if piece_in_board != nil && piece_in_board.can_move_to(position_origin,position_to_go)
+		   @board[position_to_go]=piece_in_board  
+		   @board[position_origin]=nil 
+		end
+		print
 	end
 end
 class Piece
@@ -117,60 +123,6 @@ end
  "F1" => Bishop.new, 
  "G1" => Horse.new, 
  "H1" => Tower.new, 
- 
- "A2" => nil, 
- "B2" => nil,
- "C2" => nil, 
- "D2" => nil, 
- "E2" => nil,
- "F2" => nil, 
- "G2" => nil, 
- "H2" => nil,
-  
- "A3" => nil, 
- "B3" => nil,
- "C3" => nil, 
- "D3" => nil, 
- "E3" => nil,
- "F3" => nil, 
- "G3" => nil, 
- "H3" => nil,
-
- "A4" => nil, 
- "B4" => nil,
- "C4" => nil, 
- "D4" => nil, 
- "E4" => nil,
- "F4" => nil, 
- "G4" => nil, 
- "H4" => nil,
-
- "A5" => nil, 
- "B5" => nil,
- "C5" => nil, 
- "D5" => nil, 
- "E5" => nil,
- "F5" => nil, 
- "G5" => nil, 
- "H5" => nil,
-
- "A6" => nil, 
- "B6" => nil,
- "C6" => nil, 
- "D6" => nil, 
- "E6" => nil,
- "F6" => nil, 
- "G6" => nil, 
- "H6" => nil,
-
- "A7" => nil, 
- "B7" => nil,
- "C7" => nil, 
- "D7" => nil, 
- "E7" => nil,
- "F7" => nil, 
- "G7" => nil, 
- "H7" => nil,
 
  "A8" => Tower.new, 
  "B8" => Horse.new, 
