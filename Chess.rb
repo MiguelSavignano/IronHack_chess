d154805a595500d116b80eb0f1027d42013a3709
masx <migue.masx@gmail.com>
2015-04-17 00:25:34 +0200

    refactor

--- Chess.rb
+++ Chess.rb
@@ -1,78 +1,35 @@
-require 'pry'
-class NilClass
-	def draw_piece
-		"--"
-	end
-end
+
 module MoveHelper
-		def move_tower(piece_origin,piece_to_go)
+	def move_tower(piece_origin,piece_to_go)
 			  change_to_i(piece_origin,piece_to_go)
 			  @origin_vertical_move==@to_go_vertical_move || @origin_hotizontal_move==@to_go_hotizontal_move
-		end
-		def move_bishop(piece_origin,piece_to_go)
-			change_to_i(piece_origin,piece_to_go)
-			  (@to_go_vertical_move-@origin_vertical_move).abs==(@to_go_hotizontal_move-@origin_hotizontal_move).abs
-		end
-		def move_horse_circle(piece_origin,piece_to_go)
-			change_to_i(piece_origin,piece_to_go)
-			 (@to_go_vertical_move-@origin_vertical_move).abs<=2 && (@to_go_hotizontal_move-@origin_hotizontal_move).abs<=2
-		end
-		def move_king(piece_origin,piece_to_go)
-			change_to_i(piece_origin,piece_to_go)
-			 (@to_go_vertical_move-@origin_vertical_move).abs<=1 && (@to_go_hotizontal_move-@origin_hotizontal_move).abs<=1
-		end
-		def move_king(piece_origin,piece_to_go)
-			change_to_i(piece_origin,piece_to_go)
-			 (@to_go_vertical_move-@origin_vertical_move).abs<=1 && (@to_go_hotizontal_move-@origin_hotizontal_move).abs<=1
-		end
-end
-class Board
-	attr_accessor :board, :piece_to_go, :piece_to_go, :piece_in_board,:player_who_game,:player_who_wait
-	def initialize(hash_board,white_player,black_player)
-		@empty_position="-"
-		@player_who_game=white_player
-		@player_who_wait=black_player
-		@board=hash_board
 	end
-		def change_player(player_who_game)
-		@player_who_game=@player_who_wait
-		@player_who_wait=player_who_game
+	def move_bishop(piece_origin,piece_to_go)
+		change_to_i(piece_origin,piece_to_go)
+		  (@to_go_vertical_move-@origin_vertical_move).abs==(@to_go_hotizontal_move-@origin_hotizontal_move).abs
 	end
-	def print
-			puts "-"*19
-			(1..8).each do |num| num=(num-9).abs
-				puts "#{num} |#{@board["A#{num}"].draw_piece} #{@board["B#{num}"].draw_piece} #{@board["C#{num}"].draw_piece} #{@board["D#{num}"].draw_piece} #{@board["E#{num}"].draw_piece} #{@board["F#{num}"].draw_piece} #{@board["G#{num}"].draw_piece} #{@board["H#{num}"].draw_piece}|"
-			end
-			puts "-"*19
-			puts "   A  B  C  D  E  F  G  H"
+	def move_horse_circle(piece_origin,piece_to_go)
+		change_to_i(piece_origin,piece_to_go)
+		 (@to_go_vertical_move-@origin_vertical_move).abs<=2 && (@to_go_hotizontal_move-@origin_hotizontal_move).abs<=2
 	end
-	def piece(piece_search)
-		piece_in_board=@board[piece_search].draw_piece
+	def move_king(piece_origin,piece_to_go)
+		change_to_i(piece_origin,piece_to_go)
+		 (@to_go_vertical_move-@origin_vertical_move).abs<=1 && (@to_go_hotizontal_move-@origin_hotizontal_move).abs<=1
 	end
-	def move_piece(position_origin,position_to_go)
-		piece_in_board=@board[position_origin]
-		other_piece=@board[position_to_go]
-		if  piece_in_board != nil && piece_in_board.color==@player_who_game.color && piece_in_board.can_move_to(position_origin,position_to_go)
-				if piece_can_kill(position_origin,position_to_go)
-		   			@board[position_to_go]=piece_in_board  
-		   			@board[position_origin]=nil
-		   			return true
-		   		end 
-		    return false
-		end
-	system "clear"
-	print
+	def move_king(piece_origin,piece_to_go)
+		change_to_i(piece_origin,piece_to_go)
+		 (@to_go_vertical_move-@origin_vertical_move).abs<=1 && (@to_go_hotizontal_move-@origin_hotizontal_move).abs<=1
 	end
-	def piece_can_kill(position_origin,position_to_go)
-		other_piece=@board[position_to_go]
-		the_piece=@board[position_origin]
-		if other_piece==nil 
-			true
-		else
-		    other_piece.color!=the_piece.color
-		end
+	def move_pown_white(piece_origin,piece_to_go)
+		change_to_i(piece_origin,piece_to_go)
+		  @origin_vertical_move==@to_go_vertical_move==-1
+	end
+	def move_pown_black(piece_origin,piece_to_go)
+		change_to_i(piece_origin,piece_to_go)
+		  @origin_vertical_move==@to_go_vertical_move==1
 	end
 end
+
 class Piece
 	attr_accessor :origin, :to_go, :origin_vertical_move ,:origin_hotizontal_move,:to_go_vertical_move, :to_go_hotizontal_move
 	attr_accessor :draw_piece,:color
@@ -84,6 +41,7 @@ class Piece
 		@to_go_hotizontal_move=piece_to_go[1].to_i 
 	end
 end
+
 class TowerWhite <Piece
 	def initialize
 		@draw_piece="wR"
@@ -93,12 +51,14 @@ class TowerWhite <Piece
 		 self.move_tower(piece_origin,piece_to_go)
 	end
 end
+
 class TowerBlack <TowerWhite
 	def initialize
 		@draw_piece="bR"
 		@color="B"
 	end
 end
+
 class HorseWhite <Piece
 	def initialize
 		@draw_piece="wN"
@@ -108,12 +68,14 @@ class HorseWhite <Piece
 		( self.move_horse_circle(piece_origin,piece_to_go) ) && !self.move_tower(piece_origin,piece_to_go)  && !self.move_bishop(piece_origin,piece_to_go)
 	end
 end
+
 class HorseBlack <HorseWhite
 	def initialize
 		@draw_piece="bN"
 		@color="B"
 	end
 end
+
 class BishopWhite <Piece
 	def initialize
 		@draw_piece="wB"
@@ -123,12 +85,14 @@ class BishopWhite <Piece
 		 self.move_bishop(piece_origin,piece_to_go)
 	end
 end
+
 class BishopBlack <BishopWhite
 	def initialize
 		@draw_piece="bB"
 		@color="B"
 	end
 end
+
 class QueenWhite <Piece
 	def initialize
 		@draw_piece="wQ"
@@ -138,12 +102,14 @@ class QueenWhite <Piece
 		self.move_tower(piece_origin,piece_to_go) || self.move_bishop(piece_origin,piece_to_go)
 	end
 end
+
 class QueenBlack <QueenWhite
 	def initialize
 		@draw_piece="bQ"
 		@color="B"
 	end
 end
+
 class KingWhite <Piece
 	def initialize
 		@draw_piece="wK"
@@ -153,12 +119,14 @@ class KingWhite <Piece
 		self.move_king(piece_origin,piece_to_go)
 	end
 end
+
 class KingBlack <KingWhite
 	def initialize
 		@draw_piece="bK"
 		@color="B"
 	end
 end
+
 class PownWhite <Piece
 	def initialize
 		@draw_piece="wP"
@@ -168,6 +136,7 @@ class PownWhite <Piece
 		self.move_king(piece_origin,piece_to_go)
 	end
 end
+
 class PownBlack <Piece
 	def initialize
 		@draw_piece="bP"
@@ -177,6 +146,43 @@ class PownBlack <Piece
 		self.move_king(piece_origin,piece_to_go)
 	end
 end
+
+class Player1
+	attr_accessor :color
+	def initialize
+		@color="W"
+	end
+	def ask_what_want_move
+		puts "Player1: what piece you want to move?"
+		gets.chomp
+	end
+	def ask_where_want_move
+		puts "Where piece you want to move?"
+		gets.chomp
+	end
+end
+
+class Player2
+	attr_accessor :color
+	def initialize
+		@color="B"
+	end
+	def ask_what_want_move
+		puts "Player2: what piece you want to move?"
+		gets.chomp
+	end
+	def ask_where_want_move
+		puts "Where piece you want to move?"
+		gets.chomp
+	end
+end
+
+class Ouput
+	def print_text(text)
+		puts text
+	end
+end
+
  INITIALIZE_BOARD = {
  "A1" => TowerWhite.new, 
  "B1" => HorseWhite.new,
@@ -196,39 +202,61 @@ end
  "G8" => HorseBlack.new, 
  "H8" => TowerBlack.new
 }
-class Player1
-	attr_accessor :color
-	def initialize
-		@color="W"
-	end
-	def ask_what_want_move
-		puts "Player1: what piece you want to move?"
-		gets.chomp
-	end
-	def ask_where_want_move
-		puts "Where piece you want to move?"
-		gets.chomp
+
+class NilClass
+	def draw_piece
+		"--"
 	end
 end
-class Player2
-	attr_accessor :color
-	def initialize
-		@color="B"
+
+class Board
+	attr_accessor :board, :piece_to_go, :piece_to_go, :piece_in_board,:player_who_game,:player_who_wait
+	def initialize(hash_board,white_player,black_player)
+		@empty_position="-"
+		@player_who_game=white_player
+		@player_who_wait=black_player
+		@board=hash_board
 	end
-	def ask_what_want_move
-		puts "Player2: what piece you want to move?"
-		gets.chomp
+	def print
+			puts "-"*19
+			(1..8).each do |num| num=(num-9).abs
+				puts "#{num} |#{@board["A#{num}"].draw_piece} #{@board["B#{num}"].draw_piece} #{@board["C#{num}"].draw_piece} #{@board["D#{num}"].draw_piece} #{@board["E#{num}"].draw_piece} #{@board["F#{num}"].draw_piece} #{@board["G#{num}"].draw_piece} #{@board["H#{num}"].draw_piece}|"
+			end
+			puts "-"*19
+			puts "   A  B  C  D  E  F  G  H"
 	end
-	def ask_where_want_move
-		puts "Where piece you want to move?"
-		gets.chomp
+	def change_player(player_who_game)
+		@player_who_game=@player_who_wait
+		@player_who_wait=player_who_game
+	end
+	def piece(piece_search)
+		piece_in_board=@board[piece_search].draw_piece
+	end
+	def move_piece(position_origin,position_to_go)
+		piece_in_board=@board[position_origin]
+		other_piece=@board[position_to_go]
+		if  piece_in_board != nil && piece_in_board.color==@player_who_game.color && piece_in_board.can_move_to(position_origin,position_to_go)
+			if piece_can_kill(position_origin,position_to_go)
+	   			@board[position_to_go]=piece_in_board  
+	   			@board[position_origin]=nil
+	   			return true
+	   		end 
+		    return false
+		end
+		system "clear"
+		print
+	end
+	def piece_can_kill(position_origin,position_to_go)
+		other_piece=@board[position_to_go]
+		the_piece=@board[position_origin]
+		if other_piece==nil 
+			true
+		else
+		    other_piece.color!=the_piece.color
+		end
 	end
 end
-class Ouput
-	def print_text(text)
-		puts text
-	end
-end
+
 board=Board.new(INITIALIZE_BOARD,Player1.new,Player2.new)
 output=Ouput.new
 while 1==1
@@ -241,7 +269,4 @@ while 1==1
 		output.print_text "Ilegal move"
 	end
 end
-binding.pry
-#board.print print board instance
-#board.piece("B1") search piece in board
-#board.move_piece("A1","A2") board move
+#board.move_piece("A1","A2")
